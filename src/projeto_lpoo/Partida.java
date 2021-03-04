300ca58b2815740256f7c3484a59fbe24ffafdb1
Henrique Pereira <henrique802011@gmail.com>
2021-03-04 10:56:53 -0300

    alterações feitas na classe Partida

--- src/projeto_lpoo/Partida.java
+++ src/projeto_lpoo/Partida.java
@@ -7,35 +7,61 @@ public class Partida {
 	public Tabuleiro tabuleiro = new Tabuleiro(4);
 	Scanner scan = new Scanner(System.in);
 	int jogadorVez = 1;
+	int numeroJogadas = 1;
 	
 	public void fazerJogada(int x, int y)
 	{
 		
 	}
 	
-	
-	public void fazerPartida()
+	public boolean validarJogada(int linha, int coluna)
 	{
-		int linha = 0, coluna = 0;
 		boolean isCorrect = false; 
-		
-		System.out.println("Vez do jogador " + jogadorVez + " indique a posição das pesças:");
-		
 		while(!isCorrect)
 		{
-			linha = scan.nextInt();
-			coluna = scan.nextInt();
 			if((linha>=0&&linha<=3) && (coluna>=0 && coluna<=3))
 			{
 				isCorrect = true;
 			} else {
-				System.out.println("Valor maior que o tabuleiro, jogue novamente!");
+				return false;
 			}
 		}
 		
-		System.out.println("Jogador " + jogadorVez + " jogou, X: " + linha + " Y: " + coluna);
+		return true;
+	}
+	
+	
+	public void fazerPartida()
+	{
+		int linha = 0, coluna = 0;
+		//boolean isCorrect = false; 
+		
+		while(numeroJogadas < 3)
+		{
+			System.out.println("Jogador " + jogadorVez + " indique a posição da jogada " + numeroJogadas);
+			linha = scan.nextInt();
+			coluna = scan.nextInt();
+			if(validarJogada(linha, coluna))
+			{
+				
+			} else {
+				System.out.println("Valor maior que o tabuleiro, jogue novamente!");
+				linha = scan.nextInt();
+				coluna = scan.nextInt();
+				while(!validarJogada(linha, coluna))
+				{
+					System.out.println("Valor maior que o tabuleiro, jogue novamente!");
+					linha = scan.nextInt();
+					coluna = scan.nextInt();
+				}
+			}
+			
+			System.out.println("Jogador " + jogadorVez + " jogou, X: " + linha + " Y: " + coluna);
+			numeroJogadas++;
+		}	
 		
 		jogadorVez++;
+		numeroJogadas = 1;
 		if(jogadorVez == 3)
 		{
 			jogadorVez = 1;
@@ -43,4 +69,15 @@ public class Partida {
 		
 		
 	}
+	
+	/*while(!isCorrect)
+	{
+		
+		if((linha>=0&&linha<=3) && (coluna>=0 && coluna<=3))
+		{
+			isCorrect = true;
+		} else {
+			System.out.println("Valor maior que o tabuleiro, jogue novamente!");
+		}
+	}*/
 }
