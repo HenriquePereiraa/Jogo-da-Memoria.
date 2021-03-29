a0403be6fa7ecc6af2c1d84db8804e8f9409b89e
Henrique Pereira <henrique802011@gmail.com>
2021-03-29 16:27:46 -0300

    alterações na classe tabuleiro

--- src/projeto_lpoo/Main.java
+++ src/projeto_lpoo/Main.java
@@ -5,14 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Partida start = new Partida();
+		Tabuleiro tb = new Tabuleiro(4);
 		Jogador player1 = new Jogador("Player1");
 		Jogador player2 = new Jogador("Player2");
 
 		
-		for(int i = 0; i < 6; i++)
+		/*for(int i = 0; i < 6; i++)
 		{
 			start.fazerPartida();
-		}
+		}*/
+		
+		tb.printTabuleiro();
 		
 
 	}
